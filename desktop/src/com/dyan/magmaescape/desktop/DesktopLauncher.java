9f157cb5fb3afb2feb0ee5a1410f7c8fd946c131
daniel5115 <daniel811815@gmail.com>
2021-03-16 12:16:59 -0600

    Cambios Configuracion

--- desktop/src/com/dyan/magmaescape/desktop/DesktopLauncher.java
+++ desktop/src/com/dyan/magmaescape/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.dyan.magmaescape.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.dyan.magmaescape.MyGdxGame;
+import com.dyan.magmaescape.Juego;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new Juego(), config);
 	}
 }
