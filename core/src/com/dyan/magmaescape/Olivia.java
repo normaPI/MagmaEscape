63d06b69d2be4f5d394bf2afd72202c55d16ca22
daniel5115 <daniel811815@gmail.com>
2021-04-22 13:55:44 -0500

    Olivia walking sprites

--- core/src/com/dyan/magmaescape/Olivia.java
+++ core/src/com/dyan/magmaescape/Olivia.java
@@ -1,13 +1,40 @@
 package com.dyan.magmaescape;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Olivia extends Objeto {
+    private Animation<TextureRegion> animacionCorrer;
+    private float timerAnimacion;  //Para saber el que corresponder mostar
 
 
     public Olivia(Texture textura, float x, float y){
-        super(textura,x,y);
+        TextureRegion region = new TextureRegion(textura);
+        TextureRegion[][] texturas = region.split(407,400);
 
+        //Cuadros para caminar
+        TextureRegion[] arrFramesCaminar={ texturas[0][0], texturas[0][1],texturas[1][0],texturas[1][1], texturas[2][0],texturas[2][1]};
+        animacionCorrer= new Animation<TextureRegion>(0.2f,arrFramesCaminar );
+        animacionCorrer.setPlayMode(Animation.PlayMode.LOOP);
+        timerAnimacion = 0;
+
+        //IDLE
+        sprite= new Sprite(texturas[0][0]);
+        sprite.setPosition(x,y);
+
+
+
+    }
+    //Reescribimos el metodo render
+    public void render(SpriteBatch batch){
+        float delta= Gdx.graphics.getDeltaTime();
+        timerAnimacion+=delta;
+        TextureRegion frame= animacionCorrer.getKeyFrame(timerAnimacion);
+        batch.draw(frame,sprite.getX(),sprite.getY());
 
     }
 
